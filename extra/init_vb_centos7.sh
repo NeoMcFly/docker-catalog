9cea4aec98b34bbc2ba40bb54f9e3cd04e0c92a5
Benoit <benoit.theunissen@gmail.com>
2015-11-01 22:58:25 +0100

    init script

--- extra/init_vb_centos7.sh
+++ extra/init_vb_centos7.sh
@@ -10,8 +10,10 @@ mount /dev/cdrom /mnt
 
 /mnt/VBoxLinuxAdditions.run
 
-## Docker
+## Install Docker
 yum install -y docker
+## Docker to start at boot
+sudo systemctl enable docker
 
 ## Docker Compose
 curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
@@ -23,14 +25,15 @@ docker-compose --version
 ##
 sudo firewall-cmd --zone=public --add-port=443/udp --permanent
 sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8181/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=8081/tcp --permanent
 sudo firewall-cmd --reload 
+
+# Enlever la secu sur les fichiers depuis unLXC
+# /!\ Necesiste un reboot
+sed -n 's/^SELINUX.*=.*/SELINUX=disabled/' /etc/selinux/config
 
 ## Start Docker
 sudo service docker start
-
-# Enlever la secu sur les fichiers depuis unLXC
-su -c "setenforce 0"
 
 echo "Intialisation done"
 
